80f04d1f98220371004218d51cecf823952ea872
jielenovo <109793750+jielenovo@users.noreply.github.com>
2022-11-10 20:17:30 +0800

    Add files via upload

--- wsbug.sh
+++ wsbug.sh
@@ -13,6 +13,43 @@ domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
 fi
+# MENU VLESS WS 
+echo -e "\e[0m                                                       "
+echo -e "\e[94m    .----------------------------------------------------.    "
+echo -e "\e[94m    |                    V2RAY VLESS MENU                |    "
+echo -e "\e[94m    '----------------------------------------------------'    "
+echo -e ""
+echo -e "$RED VLess Non TLS      : $(grep -c -E "^###" "/etc/v2ray/vnone.json")${NC}"
+echo -e "$RED VLess TLS          : $(grep -c -E "^###" "/etc/v2ray/vless.json")${NC}"
+echo -e ""
+echo -e "\e[1;31m* [1]\e[0m \e[1;32m: Create V2RAY Vless Websocket Account\e[0m"
+echo -e "\e[1;31m* [2]\e[0m \e[1;32m: Deleting V2RAY Vless Websocket Account\e[0m"
+echo -e "\e[1;31m* [3]\e[0m \e[1;32m: Extending VLESS Account Active Life\e[0m"
+echo -e "\e[1;31m* [4]\e[0m \e[1;32m: Check User Login V2RAY VLESS\e[0m"
+echo -e ""
+echo -e ""
+read -p "        Select From Options [1-4 or x]: " menuvless
+echo -e ""
+case $menuvless in
+1)
+add-vless
+;;
+2)
+del-vless
+;;
+3)
+renew-vless
+;;
+4)
+cek-vless
+;;
+x)
+menu
+;;
+*)
+echo " Please enter an correct number!!"
+;;
+esac
 echo -e ""
 echo -e " ${ORANGE}══════════════════════════════════════════════════════════════════${NC}"
 echo -e " ${BGBLUE}                     ┃ V2RAY/VLESS ┃                              ${NC}"
@@ -40,14 +77,14 @@ sed -i '/#tls$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vless.json
 sed -i '/#none$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vnone.json
-vlesslink1="vless://${uuid}@ufuture.uitm.edu.my:443?path=wss%3A%2F%2Fufuture.uitm.edu.my%2Fxray-vlessws-tls&sni=ufuture.uitm.edu.my&host=${domain}&security=tls&encryption=none&type=ws&sni=ufuture.uitm.edu.my#${user}"
-vlesslink2="vless://${uuid}@www.ukm.my.${domain}:$none?path=/xray-vlessws-none-tls&encryption=none&host=app.maxis.com.my&type=ws#${user}"
-vlesslink3="vless://${uuid}@onlinepayment.celcom.com.my:443?path=wss%3A%2F%2Fonlinepayment.celcom.com.my%2Fxray-vlessws-tls&sni=onlinepayment.celcom.com.my&host=${domain}&security=tls&encryption=none&type=ws&sni=onlinepayment.celcom.com.my#${user}"
-vlesslink4="vless://${uuid}@tv.iqiyi.site:$none?path=/xray-vlessws-none-tls&encryption=none&host=${domain}&type=ws#${user}"
-vlesslink5="vless://${uuid}@m.pokemon.com.${domain}:443?path=/xray-vlessws-tls&sni=m.pokemon.com&host=m.pokemon.com&security=tls&encryption=none&type=ws&sni=m.pokemon.com#${user}"
-vlesslink6="vless://${uuid}@cdn.who.int:$none?path=wss%3A%2F%2Fcdn.who.int%2Fxray-vlessws-none-tls&encryption=none&host=${domain}&type=ws#${user}"
-vlesslink7="vless://${uuid}@${sts}${domain}:443?path=/xray-vlessws-tls&sni=maingame.com.my&host=maingame.com.my&security=tls&encryption=none&type=ws&sni=maingame.com.my#${user}"
-vlesslink8="vless://${uuid}@esports.pubgmobile.com.${domain}:443?path=/xray-vlessws-tls&sni=esports.pubgmobile.com&host=esports.pubgmobile.com&security=tls&encryption=none&type=ws&sni=esports.pubgmobile.com#${user}"
+vlesslink1="vless://${uuid}@ufuture.uitm.edu.my:$tls?path=wss%3A%2F%2Fufuture.uitm.edu.my%2Fv2ray&sni=ufuture.uitm.edu.my&host=${domain}&security=tls&encryption=none&type=ws&sni=ufuture.uitm.edu.my#${user}"
+vlesslink2="vless://${uuid}@www.ukm.my.${domain}:$none?path=/v2ray&encryption=none&host=app.maxis.com.my&type=ws#${user}"
+vlesslink3="vless://${uuid}@onlinepayment.celcom.com.my:$tls?path=wss%3A%2F%2Fonlinepayment.celcom.com.my%2Fv2ray&sni=onlinepayment.celcom.com.my&host=${domain}&security=tls&encryption=none&type=ws&sni=onlinepayment.celcom.com.my#${user}"
+vlesslink4="vless://${uuid}@tv.iqiyi.site:$none?path=/v2ray&encryption=none&host=${domain}&type=ws#${user}"
+vlesslink5="vless://${uuid}@m.pokemon.com.${domain}:$tls?path=/v2ray&sni=m.pokemon.com&host=m.pokemon.com&security=tls&encryption=none&type=ws&sni=m.pokemon.com#${user}"
+vlesslink6="vless://${uuid}@cdn.who.int:$none?path=wss%3A%2F%2Fcdn.who.int%2Fv2ray&encryption=none&host=${domain}&type=ws#${user}"
+vlesslink7="vless://${uuid}@${sts}${domain}:$tls?path=/v2ray&sni=maingame.com.my&host=maingame.com.my&security=tls&encryption=none&type=ws&sni=maingame.com.my#${user}"
+vlesslink8="vless://${uuid}@esports.pubgmobile.com.${domain}:$tls?path=/v2ray&sni=esports.pubgmobile.com&host=esports.pubgmobile.com&security=tls&encryption=none&type=ws&sni=esports.pubgmobile.com#${user}"
 systemctl restart v2ray@vless
 systemctl restart v2ray@vnone
 clear
@@ -103,39 +140,3 @@ echo -e "\e[$line═════════════════════
 echo -e "Created   : $hariini"
 echo -e "Expired   : $exp"
 # MENU VLESS WS
-echo -e "\e[0m                                                       "
-echo -e "\e[94m    .----------------------------------------------------.    "
-echo -e "\e[94m    |                    V2RAY VLESS MENU                |    "
-echo -e "\e[94m    '----------------------------------------------------'    "
-echo -e ""
-echo -e "$RED VLess Non TLS      : $(grep -c -E "^###" "/etc/v2ray/vnone.json")${NC}"
-echo -e "$RED VLess TLS          : $(grep -c -E "^###" "/etc/v2ray/vless.json")${NC}"
-echo -e ""
-echo -e "\e[1;31m* [1]\e[0m \e[1;32m: Create V2RAY Vless Websocket Account\e[0m"
-echo -e "\e[1;31m* [2]\e[0m \e[1;32m: Deleting V2RAY Vless Websocket Account\e[0m"
-echo -e "\e[1;31m* [3]\e[0m \e[1;32m: Extending VLESS Account Active Life\e[0m"
-echo -e "\e[1;31m* [4]\e[0m \e[1;32m: Check User Login V2RAY VLESS\e[0m"
-echo -e ""
-echo -e ""
-read -p "        Select From Options [1-4 or x]: " menuvless
-echo -e ""
-case $menuvless in
-1)
-add-vless
-;;
-2)
-del-vless
-;;
-3)
-renew-vless
-;;
-4)
-cek-vless
-;;
-x)
-menu
-;;
-*)
-echo " Please enter an correct number!!"
-;;
-esac
